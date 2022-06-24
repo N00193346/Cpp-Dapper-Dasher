d897d675468d6fbbee17a9210da1c1719279dd6c
Paul Doyle <pauldoyle91@gmail.com>
2022-06-24 15:38:13 +0100

    Added gravity and ability to jump, stopped rectangle from falling through screen and fixed jump spam

--- dasher.cpp
+++ dasher.cpp
@@ -5,11 +5,24 @@ int main()
 {
 
     //Window dimensions
-    const int width {513};
-    const int height {380};
+    const int windowWidth {513};
+    const int windowHeight {380};
 
     //Initalise window
-    InitWindow(width, height, "Dapper Dasher");
+    InitWindow(windowWidth,windowHeight, "Dapper Dasher");
+
+    //Acceleration due to gravity (pixels/frame)/frame
+    const int gravity{1};
+    const int jumpVel{-22};
+
+    bool isInAir{false};
+
+    //Rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -18,8 +31,35 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+ 
+        //Check if on the ground
+        if (posY >= windowHeight - height) 
+        {
+            //rectangle on the ground
+            velocity = 0;
+            isInAir = false;
+        } else
+        {
+            //Apply gravity
+            velocity += gravity;
+        }
+
+        //Check for jumping
+          if(IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            velocity += jumpVel;
+            isInAir = true;
+        }
+
+        //Update position
+        posY += velocity;
+
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
         
+       
+
         EndDrawing();
     }
+    CloseWindow();
 
 }
